102a48a9893e39aa418651da0f83a91f9b0dc352
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-04-30 13:08:03 +0700

    Create saya.py

---
+++ userbot/modules/saya.py
@@ -0,0 +1,58 @@
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.sadboy(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(2)
+    await typew.edit("`Pertama-tama kamu cantik`")
+    sleep(2)
+    await typew.edit("`Kedua kamu manis`")
+    sleep(1)
+    await typew.edit("`Dan yang terakhir adalah kamu bukan jodohku`")
+# Create by myself @localheart
+
+
+@register(outgoing=True, pattern='^.punten(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n┻┳|―-∩`"
+                     "`\n┳┻|     ヽ`"
+                     "`\n┻┳|    ● |`"
+                     "`\n┳┻|▼) _ノ`"
+                     "`\n┻┳|￣  )`"
+                     "`\n┳ﾐ(￣ ／`"
+                     "`\n┻┳T￣|`"
+                     "\n**Punten**")
+
+
+@register(outgoing=True, pattern='^.pantau(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n┻┳|―-∩`"
+                     "`\n┳┻|     ヽ`"
+                     "`\n┻┳|    ● |`"
+                     "`\n┳┻|▼) _ノ`"
+                     "`\n┻┳|￣  )`"
+                     "`\n┳ﾐ(￣ ／`"
+                     "`\n┻┳T￣|`"
+                     "\n**Masih Ku Pantau**")
+
+
+# Create by myself @localheart
+
+CMD_HELP.update({
+    "saya":
+    "⚡CMD⚡`.saya`\
+    \nUsage: alive bot.\
+    \n\n⚡CMD⚡`.sadboy`\
+    \nUsage: hiks\
+    \n\n⚡CMD⚡`.punten` ; ⚡CMD⚡`.pantau`\
+    \nUsage: coba aja.\
+    \n\n⚡CMD⚡`kosong`\
+    \nUsage: tunggu update selanjutnya.\
+    \n\n⚡CMD⚡`kosong`\
+    \nUsage: tunggu update selanjutnya."
+})
