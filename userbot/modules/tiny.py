edc19dcdd7d6d8e591dafd161ccea1fe072f8a86
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-05-10 13:34:45 +0700

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -23,7 +23,7 @@ async def _(event):
         return
     xx = await event.edit("`Melakukan Aksi Haram....`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("LinuxUserbot/linuxuserbot.png")
+    im1 = Image.open("Linux/Linux.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
