0fd6e8aa44a31f375cb000106cec4951453b1b2b
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-05-24 09:35:26 +0700

    Create nyindir.py

---
+++ userbot/modules/nyindir.py
@@ -0,0 +1,30 @@
+from time import sleep
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.tua(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**EH BOCAH TUA HINA, KALO TUANYA TUA AJA GOBLOK GA COCOK BOCAH TUA KAYA LU MAIN KEK GINIAN TOLOL UDAH BAU TANAH JADINYA GAUSAH BELAGU KONTOL.**")
+    sleep(5)
+    await typew.edit("**URUSIN AJA BEGO KUBURAN LU ITU BESOK CALON RUMAH LU SEKALI SEKALI YEKAN LU TINGGAL DIRUMAH GA DI RONGSOKAN MULU**")
+
+
+@register(outgoing=True, pattern='^.gajelas(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("**YA AMPUN LU NGOMONG APA? GA NYAMBUNG KONTOL KAYA KEHIDUPAN LU MAKANYA ORG ORG KAYA LU GABAKALN MAJU HIDUPNYA APA LAGI ORG ORG BAWAHAN KAYA LU.**")
+    
+
+CMD_HELP.update({
+    "nyindir1":
+    "`•🐧CMD🐧: .tua`\
+    \n•Penjelasan: Gatau cek sendiri asu\
+    \n\n`•🐧CMD🐧: .gajelas`\
+    \n•Penjelasan: Nyindir orang Goblok."
+
+
+})
