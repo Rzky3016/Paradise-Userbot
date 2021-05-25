ebc11f085962b0264cce8a8b92c754651a105ae3
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-05-26 03:14:19 +0700

    Update alamatpalsu.py

--- userbot/modules/alamatpalsu.py
+++ userbot/modules/alamatpalsu.py
@@ -24,15 +24,15 @@ async def _(event):
     result = result.decode()
 
     result = json.loads(result)
-    Linux1 = result["type"]
-    Linux2 = result["country_code"]
-    Linux3 = result["region_name"]
-    Linux4 = result["city"]
-    Linux5 = result["zip"]
-    Linux6 = result["latitude"]
-    Linux7 = result["longitude"]
+    linux1 = result["type"]
+    linux2 = result["country_code"]
+    linux3 = result["region_name"]
+    linux4 = result["city"]
+    linux5 = result["zip"]
+    linux6 = result["latitude"]
+    linux7 = result["longitude"]
         f"<b><u>INFORMASI BERHASIL DIKUMPULKAN</b></u>\n\n<b>Ip type :-</b><code>{king1}</code>\n<b>Country code:- </b> <code>{king2}</code>\n<b>State name :-</b><code>{king3}</code>\n<b>City name :- </b><code>{king4}</code>\n<b>zip :-</b><code>{king5}</code>\n<b>Latitude:- </b> <code>{king6}</code>\n<b>Longitude :- </b><code>{king7}</code>\n",
-        parse_mode = "HTML",
+        parse_mode="HTML",
     )
 
 
