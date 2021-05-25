29fc4ae69b883195cc788fcb041a7c07ed279f46
ferikunn <custcahaya386@gmail.com>
2021-05-25 20:15:15 +0000

    pylint: auto fixes
    Signed-off-by: ferikunn <custcahaya386@gmail.com>

--- userbot/modules/alamatpalsu.py
+++ userbot/modules/alamatpalsu.py
@@ -32,7 +32,7 @@ async def _(event):
     linux6 = result["latitude"]
     linux7 = result["longitude"]
         f"<b><u>INFORMASI BERHASIL DIKUMPULKAN</b></u>\n\n<b>Ip type :-</b><code>{king1}</code>\n<b>Country code:- </b> <code>{king2}</code>\n<b>State name :-</b><code>{king3}</code>\n<b>City name :- </b><code>{king4}</code>\n<b>zip :-</b><code>{king5}</code>\n<b>Latitude:- </b> <code>{king6}</code>\n<b>Longitude :- </b><code>{king7}</code>\n",
-        parse_mode="HTML",
+        parse_mode = "HTML",
     )
 
 
