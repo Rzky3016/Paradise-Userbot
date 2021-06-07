0a009c6ad6ff082145eb39617dbc51bca788d5e2
Ferikunn <82243933+ferikunn@users.noreply.github.com>
2021-06-07 17:34:20 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,21 +1,26 @@
-# frm Ultroid
-# port by Koala @manusiarakitann
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported by Koala @manusiarakitann
 # @LordUserbot_Group
-# Alvin Ganteng
+# @sharinguserbot
 
-from userbot.events import register
 from userbot import CMD_HELP, bot
-# Alvin Ganteng
+from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.gcast (.*)")
+@register(outgoing=True, pattern=r"^\.gcast (.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if not xx:
-        return await event.edit("`Tuan, Mohon Berikan Sebuah Pesan`")
+        return await event.edit("`Berikan Sebuah Pesan`")
     tt = event.text
     msg = tt[6:]
-    kk = await event.edit("`Sedang Melakukan pengiriman Pesannn...⚡️`")
+    kk = await event.edit("`Globally Broadcasting Msg...`")
     er = 0
     done = 0
     async for x in bot.iter_dialogs():
@@ -26,11 +31,38 @@ async def gcast(event):
                 await bot.send_message(chat, msg)
             except BaseException:
                 er += 1
-    await kk.edit(f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
+    )
 
-# Alvin Ganteng
-CMD_HELP.update(
-    {
-        "gcast": "`.gcast <pesan>`\
-    \nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang Anda Masuki."
-    })
+
+@register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Berikan Sebuah Pesan`")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Globally Broadcasting Msg...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** {done} **chats**, Gagal Mengirim Pesan Ke** {er} **chats**"
+    )
+
+
+CMD_HELP.update({
+        "gcast":
+        "**•CMD:**`.gcast`\
+        \n**Penjelasan : **Mengirim  Global Broadcast pesan ke Seluruh Grup yang kamu masuk.\
+        \n\n"**•CMD:**`.gucast`\
+        \n•**Penjelasan: **Mengirim  Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk."
+})
