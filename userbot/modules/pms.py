8a8f0b1a870d33792e1afc1e48b36097e9c9dce0
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-06-13 15:02:02 +0700

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -36,7 +36,7 @@ DEF_UNAPPROVED_MSG = (
     " Jangan Bandel Dibilangin, Lu Spam Gua Blok\n"
     " Paham? Yodah Gosah Spam, Tunggu Dia Approve Chat Kamu\n"
     "╾─────────────────────╼\n"
-    "    ❗PERINGATAN 1-3❗
+    "    ❗ DIPERINGATKAN 1-3❗
     "╾─────────────────────╼\n\n"
 
 
