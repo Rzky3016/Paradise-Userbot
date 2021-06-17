ddf342e3bbe3645c50c7110d9bb376f68975f804
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-06-17 21:41:28 +0700

    Pdetect

---
+++ userbot/modules/Babu.py
@@ -0,0 +1,37 @@
+
+from telethon.errors import (
+    ChatAdminRequiredError,
+)
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+)
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.babu$")
+async def get_admin(show):
+    info = await show.client.get_entity(show.chat_id)
+    title = info.title if info.title else "Grup Ini"
+    mentions = f"<b>🐵 Daftar babu Di Group {title}:</b> \n"
+    try:
+        async for user in show.client.iter_participants(
+            show.chat_id, filter=ChannelParticipantsAdmins
+        ):
+            if not user.deleted:
+                link = f'<a href="tg://user?id={user.id}">{user.first_name}</a>'
+                mentions += f"\n🐵 Babu {link}"
+            else:
+                mentions += f"\nMonyet Meningoy <code>{user.id}</code>"
+    except ChatAdminRequiredError as err:
+        mentions += " " + str(err) + "\n"
+    await show.edit(mentions, parse_mode="html")
+
+
+CMD_HELP.update(
+    {
+        "babu": "🐵 **Cmd** : `.babu`"
+        "\n🐵 **Descriptions** : mengejek babu digrup 🐵"
+    }
+)
