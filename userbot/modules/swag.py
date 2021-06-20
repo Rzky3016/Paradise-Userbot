9c7056eacb345aa2a37781da7eb336875511b658
Rzky3016 <boski.boskiki@gmail.com>
2021-06-20 08:59:03 +0000

    pylint: auto fixes
    Signed-off-by: Rzky3016 <boski.boskiki@gmail.com>

--- userbot/modules/swag.py
+++ userbot/modules/swag.py
@@ -2,7 +2,7 @@ from userbot import bot
 from telethon import events
 
 
-@bot.on(events.NewMessage(pattern='^.swag(?: |$)(.*)", outgoing=True))
+@bot.on(events.NewMessage(pattern='^ .swag(?: |$)(.*)", outgoing=True))
 async def _(event):
 
     if event.fwd_from:
