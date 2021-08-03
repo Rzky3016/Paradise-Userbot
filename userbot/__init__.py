b125cd88906c8e56129970051cadc6c76ef713a2
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-08-04 01:41:07 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -103,7 +103,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Rzky3016/Paradise-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Linux-Userbot")
+    "UPSTREAM_REPO_BRANCH", "Paradise-Userbot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
