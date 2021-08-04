00d00b795da059ae629bca8abe0aa9224556e5f0
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-08-04 16:07:02 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🗿Paradise-Userbot🗿 ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"🗿Paradise-Userbot🗿 ⚙️ V{BOT_VER} [ACTIVED!]")
 
 
 if len(argv) not in (1, 3, 4):
