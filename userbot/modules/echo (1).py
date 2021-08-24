b12e93541ad2fc2cc71c75a20bd4bdfa09089dd8
Rzky3016 <boski.boskiki@gmail.com>
2021-08-24 09:06:37 +0000

    pylint: auto fixes
    Signed-off-by: Rzky3016 <boski.boskiki@gmail.com>

--- userbot/modules/echo (1).py
+++ userbot/modules/echo (1).py
@@ -53,7 +53,13 @@ async def echo(event):
     if is_echo(chat_id, user_id):
         return await edit_or_reply(event, "The user is already enabled with echo ")
     try:
-        addecho(chat_id, user_id, chat_name, user_name, user_username, chat_type)
+        addecho(
+            chat_id,
+            user_id,
+            chat_name,
+            user_name,
+            user_username,
+            chat_type)
     except Exception as e:
         await edit_delete(drgevent, f"**Error:**\n`{str(e)}`")
     else:
